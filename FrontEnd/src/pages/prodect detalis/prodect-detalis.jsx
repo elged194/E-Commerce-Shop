551fb6e11a102a3b479baaaff7b25d617f03e179
Hussein Elged <elged194@gmail.com>
2024-04-01 12:55:10 -0700

    Product Details

--- FrontEnd/src/pages/prodect detalis/prodect-detalis.jsx
+++ FrontEnd/src/pages/prodect detalis/prodect-detalis.jsx
@@ -1,23 +1,68 @@
+import "./prodect-detalis.css";
 import { useParams } from "react-router-dom";
 import { useGetOneProductQuery } from "../../Redux/pokemonAPI";
-import "./prodect-detalis.css";
-import { Box, CircularProgress } from "@mui/material";
-import { useState } from "react";
+import {
+  Badge,
+  Box,
+  Button,
+  CircularProgress,
+  IconButton,
+} from "@mui/material";
+import { useRef, useState } from "react";
+import DetailsThumb from "./DetailsThumb ";
+import { Add, Remove } from "@mui/icons-material";
+import styled from "@emotion/styled";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToCart,
+  decreasNumberItems,
+  increasNumberItems,
+} from "../../Redux/counterSlice";
 
 const ProdectDetalis = () => {
   let { id } = useParams();
   const { data, error, isLoading } = useGetOneProductQuery(id); // onle One Product
 
-  const [images, setImages] = useState({
-    img1: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,b_rgb:f5f5f5/3396ee3c-08cc-4ada-baa9-655af12e3120/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img2: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/e44d151a-e27a-4f7b-8650-68bc2e8cd37e/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img3: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/44fc74b6-0553-4eef-a0cc-db4f815c9450/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    img4: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/d3eb254d-0901-4158-956a-4610180545e5/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-  });
+  const [index, setindex] = useState(0);
+  const myRef = useRef(null);
 
-  const [activeImg, setActiveImage] = useState(images.img1);
+  const handleTab = (index) => {
+    setindex(index);
+    const images = myRef.current.children;
+    for (let i = 0; i < images.length; i++) {
+      images[i].className = images[i].className.replace("active", "");
+    }
+    images[index].className = "active";
+  };
 
-  const [amount, setAmount] = useState(1);
+  const { selectedPrpdectID, selectedPrpdect } = useSelector(
+    (state) => state.cartt
+  ); // acces to the data in store
+
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      right: 0,
+      top: 0,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: "13px 8px",
+      fontSize: "15px",
+      borderRadius: "50%",
+    },
+  }));
+
+  const dispatch = useDispatch();
+
+  // Namber of quantity
+  const productQuantity = (e) => {
+    const myProduct = selectedPrpdect.find((item) => {
+      return item.id === e.id;
+    });
+
+    return myProduct.quantity;
+  };
+
+  // --------------------------------------------------------------------------
+
   if (error) {
     return <h1>error</h1>;
   }
@@ -32,76 +77,68 @@ const ProdectDetalis = () => {
 
   if (data) {
     return (
-      <div className="flex flex-col justify-between lg:flex-row gap-16 lg:items-center">
-        <div className="flex flex-col gap-6 lg:w-2/4">
-          <img
-            src={activeImg}
-            alt=""
-            className="w-full h-full aspect-square object-cover rounded-xl"
-          />
-          <div className="flex flex-row justify-between h-24">
-            <img
-              src={images.img1}
-              alt=""
-              className="w-24 h-24 rounded-md cursor-pointer"
-              onClick={() => setActiveImage(images.img1)}
-            />
-            <img
-              src={images.img2}
-              alt=""
-              className="w-24 h-24 rounded-md cursor-pointer"
-              onClick={() => setActiveImage(images.img2)}
-            />
-            <img
-              src={images.img3}
-              alt=""
-              className="w-24 h-24 rounded-md cursor-pointer"
-              onClick={() => setActiveImage(images.img3)}
-            />
-            <img
-              src={images.img4}
-              alt=""
-              className="w-24 h-24 rounded-md cursor-pointer"
-              onClick={() => setActiveImage(images.img4)}
-            />
+      <div className="app">
+        <div className="details">
+          <div className="big-img">
+            <img src={data.imageLink[index]} alt="" />
           </div>
-        </div>
-        {/* ABOUT */}
-        <div className="flex flex-col gap-4 lg:w-2/4">
-          <div>
-            <span className=" text-violet-600 font-semibold">
-              Special Sneaker
-            </span>
-            <h1 className="text-3xl font-bold">Nike Invincible 3</h1>
-          </div>
-          <p className="text-gray-700">
-            Con un'ammortizzazione incredibile per sostenerti in tutti i tuoi
-            chilometri, Invincible 3 offre un livello di comfort elevatissimo
-            sotto il piede per aiutarti a dare il massimo oggi, domani e oltre.
-            Questo modello incredibilmente elastico e sostenitivo, è pensato per
-            dare il massimo lungo il tuo percorso preferito e fare ritorno a
-            casa carico di energia, in attesa della prossima corsa.
-          </p>
-          <h6 className="text-2xl font-semibold">$ 199.00</h6>
-          <div className="flex flex-row items-center gap-12">
-            <div className="flex flex-row items-center">
-              <button
-                className="bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl"
-                onClick={() => setAmount((prev) => prev - 1)}
-              >
-                -
-              </button>
-              <span className="py-4 px-6 rounded-lg">{amount}</span>
-              <button
-                className="bg-gray-200 py-2 px-4 rounded-lg text-violet-800 text-3xl"
-                onClick={() => setAmount((prev) => prev + 1)}
-              >
-                +
-              </button>
+
+          <div className="box">
+            <div className="row">
+              <h2>{data.productName}</h2>
+              <span>${data.price}</span>
             </div>
-            <button className="bg-violet-800 text-white font-semibold py-3 px-16 rounded-xl h-full">
-              Add to Cart
-            </button>
+            {/* <Colors colors={item.colors} /> */}
+
+            <p>{data.description}</p>
+
+            <DetailsThumb
+              images={data.imageLink}
+              tab={handleTab}
+              myRef={myRef}
+            />
+
+            {/* Add Button */}
+            {selectedPrpdectID.includes(data.id) ? (
+              <div
+                style={{
+                  display: "flex",
+                  width: "35%",
+                  marginLeft: "-20px",
+                  marginTop: "20px",
+                  alignItems: "center",
+                  justifyContent: "space-between ",
+                }}
+              >
+                <IconButton
+                  sx={{ ml: "10px" }}
+                  onClick={() => dispatch(decreasNumberItems(data))}
+                >
+                  <Remove />
+                </IconButton>
+
+                <StyledBadge
+                  badgeContent={productQuantity(data)}
+                  color="secondary"
+                />
+
+                <IconButton
+                  sx={{ mr: "10px" }}
+                  onClick={() => dispatch(increasNumberItems(data))}
+                >
+                  <Add />
+                </IconButton>
+              </div>
+            ) : (
+              <Button
+                sx={{ textTransform: "capitalize" , mt:"10px"}}
+                variant="contained"
+                color="primary"
+                onClick={() => dispatch(addToCart(data))}
+              >
+                add to Cart
+              </Button>
+            )}
           </div>
         </div>
       </div>
